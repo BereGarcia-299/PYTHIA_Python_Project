30882fa195b287b818aa53aa46b788e001b1bfbe
Berenice Garcia <berenice@Berenices-MacBook-Pro.local>
2021-05-06 14:53:06 -0600

    second commit

---
+++ plots/plot.py
@@ -0,0 +1,148 @@
+import ROOT
+from ROOT import TFile, TTree
+import numpy as np
+import sys
+
+if len(sys.argv) != 2:
+    print("USAGE: %s <input  file >")%(sys.argv [0])
+    sys.exit (1)
+histFile = sys.argv [1]
+inFile = ROOT.TFile.Open(histFile ,"READ")
+
+h_pt = inFile.Get("h_pt")
+h_phi = inFile.Get("h_phi")
+h_y = inFile.Get("h_y")
+
+h_photon_pt = inFile.Get("h_photon_pt")
+h_photon_phi = inFile.Get("h_photon_phi")
+h_photon_eta = inFile.Get("h_photon_eta")
+
+h_xJg_50 = inFile.Get("h_xJg_50")
+h_xJg_50_70 = inFile.Get("h_xJg_50_70")
+h_xJg_70_100 = inFile.Get("h_xJg_70_100")
+h_xJg_100_150 = inFile.Get("h_xJg_100_150")
+
+h_photonpt_highjetpt = inFile.Get("h_photonpt_highjetpt")
+h_photonpt_dphi = inFile.Get("h_photonpt_dphi")
+    
+h_xJg_50.Scale(1.0/h_xJg_50.Integral());
+h_xJg_50_70.Scale(1.0/h_xJg_50_70.Integral());
+h_xJg_70_100.Scale(1.0/h_xJg_70_100.Integral());
+h_xJg_100_150.Scale(1.0/h_xJg_100_150.Integral());
+
+h_pt.GetXaxis().SetTitle("Jet_pT");
+h_pt.GetYaxis().SetTitle("# entries");
+h_pt.GetXaxis().SetRange(0,100);
+c1 = ROOT.TCanvas("c1")
+h_pt.Draw()
+c1.Print("h_pt.pdf")
+
+h_y.GetXaxis().SetTitle("Jet_eta");
+h_y.GetYaxis().SetTitle("# entries");
+c2 = ROOT.TCanvas("c2")
+h_y.Draw()
+c2.Print("h_y.pdf")
+
+h_phi.GetXaxis().SetTitle("Jet_phi");
+h_phi.GetYaxis().SetTitle("# entries");
+h_phi.SetMinimum(0);
+c3 = ROOT.TCanvas("c3")
+h_phi.Draw()
+c3.Print("h_phi.pdf")
+
+h_photon_pt.GetXaxis().SetTitle("Photon_pT");
+h_photon_pt.GetYaxis().SetTitle("# entries");
+h_photon_pt.GetXaxis().SetRange(0,50);
+c4 = ROOT.TCanvas("c4")
+h_photon_pt.Draw()
+c4.Print("h_photon_pt.pdf")
+
+h_photon_eta.GetXaxis().SetTitle("Photon_eta");
+h_photon_eta.GetYaxis().SetTitle("# entries");
+c5 = ROOT.TCanvas("c5")
+h_photon_eta.Draw()
+c5.Print("h_photon_eta.pdf")
+
+h_photon_phi.GetXaxis().SetTitle("Photon_phi");
+h_photon_phi.GetYaxis().SetTitle("# entries");
+h_photon_phi.SetMinimum(0);
+c6 = ROOT.TCanvas("c6")
+h_photon_phi.Draw()
+c6.Print("h_photon_phi.pdf")
+
+h_xJg_50.GetXaxis().SetTitle("xJGamma (pT(photons) < 50 GeV)");
+h_xJg_50.GetYaxis().SetTitle("# entries");
+c7 = ROOT.TCanvas("c7")
+h_xJg_50.Draw("COLZ")
+c7.Print("h_xJg_50.pdf")
+
+h_xJg_50_70.GetXaxis().SetTitle("xJGamma (50 GeV < pT(photons) < 70 GeV)");
+h_xJg_50_70.GetYaxis().SetTitle("# entries");
+c8 = ROOT.TCanvas("c8")
+h_xJg_50_70.Draw("COLZ")
+c8.Print("h_xJg_50_70.pdf")
+
+h_xJg_70_100.GetXaxis().SetTitle("xJGamma (70 GeV < pT(photons) < 100 GeV)");
+h_xJg_70_100.GetYaxis().SetTitle("# entries");
+c9 = ROOT.TCanvas("c9")
+h_xJg_70_100.Draw("COLZ")
+c9.Print("h_xJg_70_100.pdf")
+
+h_xJg_100_150.GetXaxis().SetTitle("xJGamma (100 GeV < pT(photons) < 150 GeV)");
+h_xJg_100_150.GetYaxis().SetTitle("# entries");
+c10 = ROOT.TCanvas("c10")
+h_xJg_100_150.Draw("COLZ")
+c10.Print("h_xJg_100_150.pdf")
+
+h_photonpt_highjetpt.GetXaxis().SetTitle("Photon pT");
+h_photonpt_highjetpt.GetYaxis().SetTitle("Leading Jet pT");
+c11 = ROOT.TCanvas("c11")
+h_photonpt_highjetpt.Draw("COLZ")
+c11.Print("h_photonpt_highjetpt.pdf")
+
+h_photonpt_dphi.GetXaxis().SetTitle("Photon pT");
+h_photonpt_dphi.GetYaxis().SetTitle("Delta Phi between photons and leading jet");
+c12 = ROOT.TCanvas("c12")
+h_photonpt_dphi.Draw("COLZ")
+c12.Print("h_photonpt_dphi.pdf")
+
+
+
+'''
+
+    TCanvas* c1 = new TCanvas("c1","New Canvas",50,50,700,600);
+    TPad* thePad = (TPad*)c1->cd();
+    TH1F *h_frame = thePad->DrawFrame(0,0,4.0,0.15);
+    //h_frame->GetYaxis()->SetTitle("#it{v}_{2,2}");
+    //h_frame->GetYaxis()->SetTitleOffset(1.7);
+    h_frame->GetXaxis()->SetTitle("xJg for Jets with R=1.0");
+    h_frame->GetYaxis()->SetNdivisions(506,kTRUE);
+    h_frame->GetXaxis()->SetNdivisions(509,kTRUE);
+    h_frame->Draw("AXIS");
+
+    h_xJg_R1_50_70->SetLineColor(1);
+    h_xJg_R1_70_100->SetLineColor(4);
+    h_xJg_R1_100_150->SetLineColor(2);
+    h_xJg_R1_150->SetLineColor(6);
+    
+    
+    h_xJg_R1_150->Draw("hist same");
+    h_xJg_R1_100_150->Draw("hist same");
+    h_xJg_R1_50_70->Draw("hist same");
+    h_xJg_R1_70_100->Draw("hist same");
+    auto legend = new TLegend(0.1,0.7,0.48,0.9);
+    legend->SetBorderSize(0);
+    legend->SetTextSize(0.04);
+    legend->SetTextFont(42);
+    legend->AddEntry("h_xJg_R1_50_70","photon pT: [50,70] GeV","l");
+    legend->AddEntry("h_xJg_R1_70_100","photon pT: [70,100] GeV","l");
+    legend->AddEntry("h_xJg_R1_100_150","photon pT: [100,150] GeV","l");
+    legend->AddEntry("h_xJg_R1_150","photon pT > 150 GeV","l");
+    legend->Draw();
+    //myMarkerLineText(0.6,0.85, 1.2, 1, 21, 1, 1, "h_xJg_R1_50_70", 0.04, true);
+    //myMarkerLineText(0.6,0.80, 1.2, 4, 21, 4, 1, "h_xJg_R1_70_100", 0.04, true);
+    //myMarkerLineText(0.6,0.75, 1.2, 2, 21, 2, 1, "h_xJg_R1_100_150", 0.04, true);
+    //myMarkerLineText(0.6,0.70, 1.2, 6, 21, 6, 1, "h_xJg_R1_150", 0.04, true);
+
+
+'''
---
+++ test/Analysis_quark_gluon_Jets.py
@@ -0,0 +1,341 @@
+# main01.py is a part of the PYTHIA event generator.
+# Copyright (C) 2020 Torbjorn Sjostrand.
+# PYTHIA is licenced under the GNU GPL v2 or later, see COPYING for details.
+# Please respect the MCnet Guidelines, see GUIDELINES for details.
+
+# Keywords: basic usage; charged multiplicity; python;
+
+# This is a simple test program. It fits on one slide in a talk.  It
+# studies the charged multiplicity distribution at the LHC. To set the
+# path to the Pythia 8 Python interface do either (in a shell prompt):
+#      export PYTHONPATH=$(PREFIX_LIB):$PYTHONPATH
+# or the following which sets the path from within Python.
+#
+# Use "python-config --include" to find the include directory and
+# then configure Pythia ""--with-python-include=*".
+
+import sys
+
+import math 
+
+cfg = open("Makefile.inc")
+lib = "../lib"
+for line in cfg:
+    if line.startswith("PREFIX_LIB="): lib = line[11:-1]; break
+sys.path.insert(0, lib)
+
+#following https://wiki.physik.uzh.ch/cms/root:pyroot_ttree
+#IMPORT root
+from ROOT import TFile, TTree
+from array import array
+
+inFile = TFile("tree.root", 'recreate')
+inTree = TTree("tree_name", "tree title")
+
+nPart= array('i',[0])
+bigSize=10000000
+pxVar = array('d',[0])
+pyVar = array('d',[0])
+pzVar = array('d',[0])
+eVar = array('d',[0])
+mVar = array('d',[0])
+idVar = array('i',[0])
+
+nJets = array('i',[0])
+jets_phi = array('f',[0])
+jets_pT =array('f',[0])
+jets_m =array('f',[0])
+jets_eta=array('f',[0])	
+jets_px=array('f',[0])
+jets_py=array('f',[0])
+jets_pz=array('f',[0])
+jets_e=array('f',[0])
+
+nGamma = array('i',[0])
+gamma_pT=array('d',[0])
+gamma_phi=array('d',[0])
+gamma_eta=array('d',[0])
+
+nGluonJets = array('i',[0])
+gluon_jets_pT = array('f',[0])
+gluon_jets_phi = array('f',[0])
+gluon_jets_eta = array('f',[0])
+
+nQuarkJets = array('i',[0])
+quark_jets_pT = array('f',[0])
+quark_jets_phi = array('f',[0])
+quark_jets_eta = array('f',[0])
+
+keep_jet_indicies = array('i',[0])
+
+for i in range(0,bigSize):
+    pxVar.append(0)
+    pyVar.append(0)
+    pzVar.append(0)
+    eVar.append(0)
+    mVar.append(0)
+    idVar.append(0)
+    jets_phi.append(0)
+    jets_pT.append(0)
+    jets_m.append(0)
+    jets_eta.append(0)
+    jets_px.append(0)
+    jets_py.append(0)
+    jets_pz.append(0)
+    jets_e.append(0)
+    gamma_pT.append(0)
+    gamma_phi.append(0)
+    gamma_eta.append(0)	
+    gluon_jets_pT.append(0)
+    gluon_jets_phi.append(0)
+    gluon_jets_eta.append(0)	
+    quark_jets_pT.append(0)
+    quark_jets_phi.append(0)
+    quark_jets_eta.append(0)
+
+
+for i in range(0,90):
+    keep_jet_indicies.append(-1)
+
+
+inTree.Branch("nPart",  nPart,  'nPart/I')
+inTree.Branch("px",  pxVar,  'px[nPart]/D')
+inTree.Branch("py",  pyVar,  'py[nPart]/D')
+inTree.Branch("pz",  pzVar,  'pz[nPart]/D')
+inTree.Branch("e",  eVar,  'e[nPart]/D')
+inTree.Branch("m",  mVar,  'm[nPart]/D')
+inTree.Branch("id",  idVar,  'id[nPart]/I')
+inTree.Branch("nJets", nJets, 'nJets/I')
+inTree.Branch("jets_phi",jets_phi,'jets_phi[nJets]/F')
+inTree.Branch("jets_pT",jets_pT,'jets_pT[nJets]/F')
+inTree.Branch("jets_m",jets_m,'jets_m[nJets]/F')
+inTree.Branch("jets_eta",jets_eta,'jets_eta[nJets]/F')
+inTree.Branch("jets_px",jets_px,'jets_px[nJets]/F')
+inTree.Branch("jets_py",jets_py,'jets_py[nJets]/F')
+inTree.Branch("jets_pz",jets_pz,'jets_pz[nJets]/F')
+inTree.Branch("jets_e",jets_e,'jets_e[nJets]/F')
+
+inTree.Branch("nGamma",nGamma,'nGamma/I')
+inTree.Branch("gamma_pT",gamma_pT,'gamma_pT[nGamma]/F')
+inTree.Branch("gamma_phi",gamma_phi,'gamma_phi[nGamma]/F')
+inTree.Branch("gamma_eta",gamma_eta,'gamma_eta[nGamma]/F')
+
+inTree.Branch("nGluonJets",nGluonJets,'nGluonJets/I')
+inTree.Branch("gluon_jets_pT",gluon_jets_pT,'gluon_jets_pT[nGluonJets]/F')
+inTree.Branch("gluon_jets_eta",gluon_jets_eta,'gluon_jets_eta[nGluonJets]/F')
+inTree.Branch("gluon_jets_phi",gluon_jets_phi,'gluon_jets_phi[nGluonJets]/F')
+
+
+inTree.Branch("nQuarkJets",nQuarkJets,'nQuarkJets/I')
+inTree.Branch("quark_jets_pT",quark_jets_pT,'quark_jets_pT[nQuarkJets]/F')
+inTree.Branch("quark_jets_eta",quark_jets_eta,'quark_jets_eta[nQuarkJets]/F')
+inTree.Branch("quark_jets_phi",quark_jets_phi,'quark_jets_phi[nQuarkJets]/F')
+
+# Import the Pythia module.
+import pythia8
+pythia = pythia8.Pythia()
+
+#Initialize the Angantyr model to fit the total and semi-includive
+#cross sections in Pythia within some tolerance.
+pythia.readString("HeavyIon:SigFitErr = "
+                    "0.02,0.02,0.1,0.05,0.05,0.0,0.1,0.0");
+#These parameters are typicall suitable for sqrt(S_NN)=5TeV
+pythia.readString("HeavyIon:SigFitDefPar = "
+                    "17.24,2.15,0.33,0.0,0.0,0.0,0.0,0.0");
+#A simple genetic algorithm is run for 20 generations to fit the parameters.
+pythia.readString("HeavyIon:SigFitNGen = 20");
+
+
+pythia.readString("HardQCD:all = off")
+pythia.readString("PromptPhoton:all = on")
+pythia.init()
+mult = pythia8.Hist("charged multiplicity", 100, -0.5, 799.5)
+# Begin event loop. Generate event. Skip if error. List first one.
+
+for iEvent in range(0, 30):
+    if not pythia.next(): continue
+    # Find number of all final charged particles and fill histogram.
+    nCharged = 0
+    nPart[0]=0
+    nJets[0]=0
+    nGamma[0]=0	 
+    nQuarkJets[0]=0
+    nGluonJets[0] = 0	     
+    nJets[0] = 0	
+   
+    #Setting Up The Jet Class
+    jets = pythia8.SlowJet(-1, 1.0, 10., 5)
+    jets.analyze(pythia.event)
+    numJets = jets.sizeJet() 
+
+    #Test:
+    #Uncomment the print of lines if you wish to run tets
+
+    #print("Event Number: ")
+    #print(iEvent)    
+    #print("This is the number of jets in this events: ")
+    #print(numJets)
+
+    for iJets in range(0,numJets):
+    	 #Running Over Jets In Events 
+	 count_gluons_inJet_5 = 0
+    	 count_quarks_inJet_5 = 0
+    	 count_quarks_inJet_6 = 0
+    	 count_gluons_inJet_6 = 0
+    	 pos = 0
+    	 passed_5 = False
+    	 passed_6 = False
+    	 for prt in pythia.event:
+    	 	#Looping Over All Particles
+	 	if((prt.idAbs()==22) and (prt.pT()>3) and (prt.pT()<800)):
+			#If Particle Made  It Here It Means It Is A Photon
+			# With a pT > 3 GeV and < 800 GeV
+			
+			#print("Collected a gamma!")
+			#print("pT: ")
+			#print(prt.pT())
+			#print("Eta value of gamma: ")
+			#print(prt.y())
+			#print("Phi value of gamma: ")
+			#print("prt.phi()")
+	
+        		#Collecting Photons Kinematic Information
+			gamma_pT[nGamma[0]]=prt.pT()
+        		gamma_eta[nGamma[0]]=prt.y()
+        		gamma_phi[nGamma[0]]=prt.phi()
+        		nGamma[0]+=1    	 	
+		
+  		#The pTHat Value Give The pT Of The Particles In The Events 
+		pTHat_val = pythia.infoPython().pTHat()
+
+    	 	
+    	 	if((pos == 1) or (pos==2)):
+    	 		#Here we are checking the particke position (initial state and final state)
+    	 		parton_mom = prt.idAbs()
+			
+    	    
+		if((pos==5) and (parton_mom ==2212)):
+			#We will save the final state of the parton
+			#(These can potentially be quarks or gluons)
+		  	part5_iD = prt.idAbs()
+			part5_pT = prt.pT()
+		  	part5_pz = prt.pz()
+		  	part5_py = prt.py()
+		  	part5_px = prt.px()
+		  	part5_eta = prt.y()
+		  	part5_phi = prt.phi()
+		  	passed_5 = True
+			
+		if((pos == 6) and (parton_mom==2212)):
+			#We will save the final state of the parton
+			#(These can potentially be quarks or gluons)
+		 	part6_iD = prt.idAbs()
+			part6_pT = prt.pT()
+		  	part6_pz = prt.pz()
+		  	part6_py = prt.py()
+		  	part6_px = prt.px()
+		  	part6_eta = prt.y()
+		  	part6_phi = prt.phi()
+		  	passed_6 = True
+		  	
+		if(passed_5 and passed_6):
+		  #It made sure to grab the particle information after the hard scattering
+		  #Between two partons
+		  sum_px = part5_px + part6_px
+		  sum_py = part5_py + part6_py
+		  #We summed the px and py values to check that they came from the same
+	          #HardScattering. IF they did, they px's and py's will add to zero
+	
+		  if((sum_py == 0) and (sum_py==0)):
+		  	#print("sum of pys and pxs is: ")
+			#print(sum_py)
+			#print(sum_px)
+			if((pTHat_val == part6_pT) and (pTHat_val == part5_pT)):
+				#In the line of code from above we are checking that the pTHat value matches 
+				#The pT of the particle will match to the pTHat if it comes from a Hardscattering event	  		
+	
+				#We are now matching jets to gluons
+		  		
+		  		
+		  		deltaEta_5J = jets.y(iJets) - part5_eta
+				deltaEta_6J = jets.y(iJets) - part6_eta
+				
+				deltaPhi_5J = jets.phi(iJets) - part5_phi 
+				deltaPhi_6J = jets.phi(iJets) - part6_phi
+
+				deltaR_5J = (deltaEta_5J**2 + deltaPhi_5J**2)**(0.5)
+				deltaR_6J = (deltaPhi_6J**2 + deltaPhi_6J**2)**(0.5)
+				
+				#We calculate the distance between the particle and jet (deltaR)
+				#Its within our jet if deltaR < 0.3 (ofcourse this can vary but this is the cut we chose)
+				if((part5_iD == 21) and (deltaR_5J<0.3) and (part5_pT > 10)):
+					if(jets.pT(iJets)>20):
+					#Requiring Jet to have a pT > 20 GeV
+						count_gluons_inJet_5 +=1
+						#Counting the number of gluons in my jet
+     		                if((part6_iD == 21) and (deltaR_6J<0.3) and (part6_pT > 10)):
+					if(jets.pT(iJets)>20):
+						count_gluons_inJet_6 +=1
+
+				if((part6_iD == 2) and (deltaR_6J<0.3) and (part6_pT > 10)):
+					if(jets.pT(iJets)>20):
+						count_quarks_inJet_6 += 1
+				if((part5_iD == 2) and (deltaR_5J<0.3) and (part5_pT >10)):
+					if(jets.pT(iJets)>20):
+						count_quarks_inJet_5 += 1
+  	
+        	pos += 1			
+			
+	 if ((count_gluons_inJet_5 > 400) or (count_gluons_inJet_6 > 400)):
+	     #print("this is how many gluons are in our jet: ")
+	     #print(count_gluons_inJet_5)
+	     #print(count_gluons_inJet_6)
+	     #If there are more than 400 gluons in my jet record it as gluon-jet	
+	     gluon_jets_pT[nGluonJets[0]] = jets.pT(iJets);
+	     gluon_jets_phi[nGluonJets[0]] =jets.phi(iJets);
+	     gluon_jets_eta[nGluonJets[0]] = jets.y(iJets);
+	     nGluonJets[0] += 1
+	   
+	 if((count_quarks_inJet_5 > 300) or (count_quarks_inJet_6>300)):
+	     #print("this is how many quarks are in our jet: ")
+	     #print(count_quarks_inJet_5)
+	     #print(count_quarks_inJet_6)
+	     #If there are more than 300 quarks in my jet record it as a quark-jet
+	     quark_jets_pT[nQuarkJets[0]] = jets.pT(iJets);
+	     quark_jets_phi[nQuarkJets[0]] = jets.phi(iJets);
+	     quark_jets_eta[nQuarkJets[0]] = jets.y(iJets);
+	     nQuarkJets[0] += 1	
+	
+	
+    #Filling my tree
+    inTree.Fill()
+    mult.fill(nCharged)
+# End of event loop. Statistics. Histogram. Done.
+pythia.stat();
+print(mult)
+
+
+inFile.Write("", TFile.kOverwrite)
+inFile.Close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
