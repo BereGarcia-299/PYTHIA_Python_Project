ac509c7b9c6eca54579df9a02be94556eefbd1a8
BereGarcia-299 <59068247+BereGarcia-299@users.noreply.github.com>
2021-05-06 15:55:33 -0600

    Update Analysis_quark_gluon_Jets.py

--- test/Analysis_quark_gluon_Jets.py
+++ test/Analysis_quark_gluon_Jets.py
@@ -1,18 +1,5 @@
-# main01.py is a part of the PYTHIA event generator.
-# Copyright (C) 2020 Torbjorn Sjostrand.
-# PYTHIA is licenced under the GNU GPL v2 or later, see COPYING for details.
-# Please respect the MCnet Guidelines, see GUIDELINES for details.
+#This is where we test our Analysis code
 
-# Keywords: basic usage; charged multiplicity; python;
-
-# This is a simple test program. It fits on one slide in a talk.  It
-# studies the charged multiplicity distribution at the LHC. To set the
-# path to the Pythia 8 Python interface do either (in a shell prompt):
-#      export PYTHONPATH=$(PREFIX_LIB):$PYTHONPATH
-# or the following which sets the path from within Python.
-#
-# Use "python-config --include" to find the include directory and
-# then configure Pythia ""--with-python-include=*".
 
 import sys
 
